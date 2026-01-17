136d005a7a9bf683f8a5ba9f8c4aa3a7abe2dd14
soltsega <tsegasolomon538@gmail.com>
2026-01-17 19:34:04 +0300

    A file to define assets adn jobs that will be used in the pipline and schedule of the pipline

---
+++ orchestration/definitions.py
@@ -0,0 +1,12 @@
+#This file is used to define the assets and jobs that will be used in the pipeline
+# it is also used to define the dependencies between the assets
+# it is also used to define the schedule for the pipeline
+
+from . import assets, jobs
+
+all_assets = load_assets_from_modules([assets])
+
+defs = Definitions(
+    assets=all_assets,
+    jobs=[jobs.daily_pipeline_job],
+)
